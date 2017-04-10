71a57d5ab317a5f1ba3b56637044f68d8e4b5534
Marcos Araujo Sobrinho <marcos.sobrinho@vivareal.com>
2017-04-10 14:51:40 -0300

    removing unused imports, fixing gitignore

--- src/test/java/com/vivareal/search/api/model/SearchRequestTest.java
+++ src/test/java/com/vivareal/search/api/model/SearchRequestTest.java
@@ -2,8 +2,6 @@ package com.vivareal.search.api.model;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
