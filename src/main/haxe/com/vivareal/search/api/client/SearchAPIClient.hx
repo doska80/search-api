0395300a180b722286dc2cd379645d65f26bc66f
Marcos Araujo Sobrinho <marcos.sobrinho@vivareal.com>
2017-04-11 17:12:10 -0300

    Haxe SDK :D

---
+++ src/main/haxe/com/vivareal/search/api/client/SearchAPIClient.hx
@@ -0,0 +1,20 @@
+package com.vivareal.search.api.client;
+
+import haxe.Http;
+import haxe.Json;
+
+class SearchAPIClient {
+
+    public function new():Void {
+    }
+
+    public function message():String {
+        return "Hello World :D";
+    }
+
+//    static public function main():Void {
+//        var searchApiClient = new SearchAPIClient();
+//        trace(searchApiClient.message());
+//    }
+
+}
