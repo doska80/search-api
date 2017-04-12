2f1e9b2c9e284ae88c37a6f40001f8042a96a026
Felipe Mamud <felipe.mamud@vivareal.com>
2017-04-12 13:49:07 -0300

    created streaming controller

---
+++ src/main/java/com/vivareal/search/api/controller/v2/stream/ResponseStream.java
@@ -0,0 +1,30 @@
+package com.vivareal.search.api.controller.v2.stream;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.function.Function;
+
+public final class ResponseStream {
+
+    private OutputStream stream;
+
+    private ResponseStream(OutputStream stream) {
+        this.stream = stream;
+    }
+
+    public static ResponseStream create(OutputStream stream) {
+        return new ResponseStream(stream);
+    }
+
+    public <T> void withIterable(Iterable<T> iterable, Function<T, byte[]> fn) throws IOException {
+        for (T t : iterable) {
+            write(fn.apply(t));
+        }
+        stream.close();
+    }
+
+    private void write(byte[] bytes) throws IOException {
+        stream.write(bytes);
+        stream.flush();
+    }
+}
--- src/test/java/com/vivareal/search/api/controller/v2/ListingControllerTest.java
+++ src/test/java/com/vivareal/search/api/controller/v2/ListingControllerTest.java
@@ -1,12 +1,21 @@
 package com.vivareal.search.api.controller.v2;
 
+import java.util.List;
 import com.vivareal.search.api.model.SearchApiResponse;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.ResponseExtractor;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
@@ -26,4 +35,18 @@ public class ListingControllerTest {
         assertNotNull(response);
         assertThat(response.getListings()).isEqualTo("foi memo!");
     }
+
+    @Test
+    public void exampleStreamTest() {
+        String list = this.restTemplate.execute("/v2/listings/stream", HttpMethod.GET, null, (ClientHttpResponse client) -> {
+            try (Scanner sc = new Scanner(client.getBody())) {
+                return sc.nextLine();
+            }
+        });
+
+        String[] listings = list.split("\\}\\{");
+        assertNotNull(list);
+        assertEquals(10, listings.length);
+    }
+
 }
