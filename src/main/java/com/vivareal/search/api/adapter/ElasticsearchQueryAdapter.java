ae6b18373c83353792c7fb45c5877b8bba0e9f36
Felipe Mamud <felipe.mamud@vivareal.com>
2017-04-24 18:02:55 -0300

    fixing import

--- src/main/java/com/vivareal/search/api/adapter/ElasticsearchQueryAdapter.java
+++ src/main/java/com/vivareal/search/api/adapter/ElasticsearchQueryAdapter.java
@@ -22,6 +22,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
