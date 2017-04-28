2f7496c7a6ce5baed08c80ec940a2202b6f2c036
Felipe Mamud <felipe.mamud@vivareal.com>
2017-04-28 13:46:31 -0300

    almost field parser :D

--- src/main/java/com/vivareal/search/api/parser/FilterParser.java
+++ src/main/java/com/vivareal/search/api/parser/FilterParser.java
@@ -1,6 +1,7 @@
 package com.vivareal.search.api.parser;
 
 import com.vivareal.search.api.model.query.Expression;
+import com.vivareal.search.api.model.query.Field;
 import org.jparsec.*;
 
 import java.util.List;
@@ -16,14 +17,28 @@ public class FilterParser {
             .keywords(new String[] { "OR", "AND", "NOT", "IN" })
             .build();
     static Parser<Expression> EXPRESSION_PARSER = Parsers.sequence(Terminals.fragment(Tokens.Tag.RESERVED).map(e -> {
-//        System.out.println(e);
+        System.out.println(e);
         return Expression.get(e);
     })).cast();
-    static final Parser<?> TOKENIZER = Parsers.or(STRING_TOKENIZER, EXPRESSION_PARSER.from(OPERATORS.tokenizer(), Scanners.WHITESPACES.optional(null)));
+
+//    static Parser<Field> FIELD_PARSER = Parsers.sequence(Terminals.fragment(Tokens.Tag.RESERVED).map(e -> {
+//        System.out.println(e);
+//        return new Field((String) null, (String)null, (String)null);
+//    })).cast();
+
+
+    static Parser<Field> FIELD_PARSER = STRING_TOKENIZER.map(c -> {
+        System.out.println(c);
+        return new Field((String) null, "eq", (String)null);
+    });
+
+    static final Parser<?> TOKENIZER = Parsers.or(FIELD_PARSER,
+            EXPRESSION_PARSER.from(OPERATORS.tokenizer(), IGNORED));
+
     static final Parser<?> PARSER = Parsers.or(TOKENIZER, IGNORED);
 
     public static void main(String[] args) {
-        System.out.println(TERMINALS.token("OR"));
+        //System.out.println(TERMINALS.token("OR"));
         List<?> parsed = PARSER.many().parse("field: value AND outroField:outroValue OR bla EQ ble AND bla EQ 'ble AND bli' AND foo='abc' AND bar<>'def' OR (biz IN ['a', 'b', 'c'] AND NOT baz = 'foo'");
         System.out.println(parsed);
     }
