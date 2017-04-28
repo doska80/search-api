90811d5c17124dbea0fefd3becb384bb40a9298a
Felipe Mamud <felipe.mamud@vivareal.com>
2017-04-28 18:11:06 -0300

    fixing value parser test removing quotes

--- src/test/java/com/vivareal/search/api/parser/ValueParserTest.java
+++ src/test/java/com/vivareal/search/api/parser/ValueParserTest.java
@@ -27,7 +27,7 @@ public class ValueParserTest {
         String unparsed = "'single-quoted and with a lot of spaces'";
         Parser<String> parser = ValueParser.get();
         String parsed = parser.parse(unparsed);
-        assertEquals(unparsed, parsed);
+        assertEquals(unparsed.substring(1, unparsed.length() - 1), parsed);
     }
 
     @Test
@@ -35,7 +35,7 @@ public class ValueParserTest {
         String unparsed = "\"single-quoted and with a lot of spaces and sôme spécial chars\"";
         Parser<String> parser = ValueParser.get();
         String parsed = parser.parse(unparsed);
-        assertEquals(unparsed, parsed);
+        assertEquals(unparsed.substring(1, unparsed.length() - 1), parsed);
     }
 
 }
