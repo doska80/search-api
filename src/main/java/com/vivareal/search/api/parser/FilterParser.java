594ba82877cd69fd289f8f15be4a827114394843
Marcos Araujo Sobrinho <marcos.sobrinho@vivareal.com>
2017-04-28 19:04:48 -0300

    removing commented code

--- src/main/java/com/vivareal/search/api/parser/FilterParser.java
+++ src/main/java/com/vivareal/search/api/parser/FilterParser.java
@@ -3,12 +3,9 @@ package com.vivareal.search.api.parser;
 import org.jparsec.Parser;
 import org.jparsec.Parsers;
 
-import java.util.List;
-
 public class FilterParser {
 
     private static final Parser<Filter> SINGLE_EXPRESSION_PARSER = Parsers.array(FieldParser.get(), ComparisonParser.get(), ValueParser.get()).map(Filter::new).cast();
-            //sequence(FieldParser.get(), ComparisonParser.get(), ValueParser.get()).many().map((List<String> field) -> new Filter(field)).cast();
 
     public static Parser<Filter> get() {
         return SINGLE_EXPRESSION_PARSER;
