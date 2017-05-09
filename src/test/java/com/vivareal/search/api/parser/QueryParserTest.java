30073d6334d26082bf7b7bd12f540cce16723972
Marcos Araujo Sobrinho <marcos.sobrinho@vivareal.com>
2017-05-09 17:11:13 -0300

    renaming test, better assertions

--- src/test/java/com/vivareal/search/api/parser/QueryParserTest.java
+++ src/test/java/com/vivareal/search/api/parser/QueryParserTest.java
@@ -1,22 +1,21 @@
 package com.vivareal.search.api.parser;
 
+import com.google.common.base.Joiner;
 import org.jparsec.Parser;
 import org.junit.Test;
 
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class QueryParserTest {
 
     Parser<List<QueryFragment>> parser = QueryParser.get();
 
     @Test
-    public void simpleTest() {
+    public void nonRecursiveMultipleConditionsTest() {
         List<QueryFragment> query = parser.parse("field1 EQ value1 AND field2 NE value2 OR field3 GT 123");
-        for (QueryFragment fragment: query) {
-            System.out.println(fragment);
-        }
+        assertEquals("field1 EQUAL value1 AND field2 DIFFERENT value2 OR field3 GREATER 123", Joiner.on(' ').join(query));
     }
 
 
