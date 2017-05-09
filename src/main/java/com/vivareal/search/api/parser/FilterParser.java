9477d4631142c7542b2595e3e42aa3c67a854d25
Marcos Araujo Sobrinho <marcos.sobrinho@vivareal.com>
2017-05-09 18:45:45 -0300

    removing explicit cast

--- src/main/java/com/vivareal/search/api/parser/FilterParser.java
+++ src/main/java/com/vivareal/search/api/parser/FilterParser.java
@@ -8,7 +8,7 @@ public class FilterParser {
     private static final Parser<Field> FIELD_PARSER = FieldParser.get();
     private static final Parser<RelationalOperator> RELATIONAL_OP_PARSER = RelationalOperatorParser.get();
     private static final Parser<Value> VALUE_PARSER = ValueParser.get();
-    private static final Parser<Filter> SINGLE_EXPRESSION_PARSER = Parsers.array(FIELD_PARSER, RELATIONAL_OP_PARSER, VALUE_PARSER).map((Object[] expression) -> new Filter(expression)).cast();
+    private static final Parser<Filter> SINGLE_EXPRESSION_PARSER = Parsers.array(FIELD_PARSER, RELATIONAL_OP_PARSER, VALUE_PARSER).map(expression -> new Filter(expression)).cast();
 
     public static Parser<Filter> get() {
         return SINGLE_EXPRESSION_PARSER;
