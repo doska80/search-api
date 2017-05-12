c2610f3f0b048237ea75f95e080d37190a5ab8e7
Marcos Araujo Sobrinho <marcos.sobrinho@vivareal.com>
2017-05-12 16:19:10 -0300

    fixing test (adding quotes), adding new test that's not working yet

--- src/test/java/com/vivareal/search/api/parser/FilterParserTest.java
+++ src/test/java/com/vivareal/search/api/parser/FilterParserTest.java
@@ -8,15 +8,22 @@ import static org.junit.Assert.assertEquals;
 public class FilterParserTest {
 
     @Test
-    public void testSingleExpression() {
+    public void testSingleExpressionWithDoubleQuotes() {
         Parser<Filter> parser = FilterParser.get();
-        parser.parse("pedrito=gringo");
+        parser.parse("pedrito=\"gringo\"");
     }
 
     @Test
-    public void testSingleExpressionWithSpaces() {
+    public void testSingleExpressionWithSpacesAndSingleQuotes() {
         Parser<Filter> parser = FilterParser.get();
         Filter filter = parser.parse("pedrito = 'gringo mardito'");
+        assertEquals("pedrito EQUAL gringo mardito", filter.toString());
+    }
+
+    @Test
+    public void testSingleExpressionWithINAndSpaces() {
+        Parser<Filter> parser = FilterParser.get();
+        Filter filter = parser.parse("pedrito IN ['gringo', 'mardito']");
         assertEquals("pedrito EQUAL gringo mardito", filter.toString());
     }
 
